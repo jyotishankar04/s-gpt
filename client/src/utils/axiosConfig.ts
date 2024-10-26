7d217f7b6089df7e3a7908e6621dfd43257544d6
jyotishankar04 <patrajyotishankar@gamil.com>
2024-10-26 21:38:51 +0530

    v1

--- client/src/utils/axiosConfig.ts
+++ client/src/utils/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:8787/api",
+  baseURL: "https://backend.patrajyotishankarlearn.workers.dev/api",
   withCredentials: true,
 });
 
